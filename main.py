dbb212abb420fbdb925919765ebe4c2b8393d9a2
kobanya <nagy.bela.budapest@gmail.com>
2023-03-20 22:12:43 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+NB-2023.03.20
+Két termék - for ciklussal
+f kiíratás
+
+
+'''
+
+
+from random import *
+
+for i in range(1,16):
+    termek=randrange(100)+1
+
+    print(f"  {i}  termék :   {termek} db")
